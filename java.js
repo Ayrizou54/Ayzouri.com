4344c2203077719d8020fccfa73429a1ef5dce11
Elhamdaoui mohammed <dandybouafssa@gmail.com>
2025-02-21 19:51:28 +0100

    Update java.js

--- java.js
+++ java.js
@@ -1,18 +1,13 @@
-const fleche = document.querySelector('.fleche');
-
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 300) {
-    fleche.style.opacity = '1';
-    fleche.style.pointerEvents = 'auto';
-  } else {
-    fleche.style.opacity = '0';
-    fleche.style.pointerEvents = 'none';
-  }
-});
-
-fleche.addEventListener('click', () => {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
-  });
-});
+const flèche= document.querySelector('.flèche');
+window.addEventListener('scroll',()=>{
+    if(window.scrollY > 300){
+        flèche.style.opacity ='1';
+        flèche.style.pointerEvents ='auto';
+      }else{
+        flèche.style.opacity= '0';
+        flèche.style.pointerEvents = 'none'; 
+      }
+      flèche.addEventListener('click',() =>{
+        window.scrollTo(300,0);
+      })
+ })
